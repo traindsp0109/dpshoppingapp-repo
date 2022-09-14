f7cafc43410ebe908020f2614979ca0006dd6961
unknown <traindsp0109@outlook.com>
2022-09-14 05:35:36 +0000

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using DPShopping.Client.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DPShopping.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(ProductContext.Products);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DPShopping.Client.MvcApplication" Language="C#" %>
